d90bb52561e9c0688017658b5a303382b6ffc1ec
Maksym Shemet <max.shemet@gmail.com>
2023-03-28 15:50:46 +0300

    add ref & reactive ts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { reactive } from "vue";
+
+import type User from "@/types/User";
+
 import TheHeader from "@/components/TheHeader.vue";
-import EntryEditor from "./components/EntryEditor.vue";
+import EntryEditor from "@/components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+
+const user: User = reactive({
+  id: 1,
+  username: "test_user",
+  settings: [],
+});
 </script>
 
 <template>
